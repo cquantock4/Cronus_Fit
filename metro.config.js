4422dc343d8082f02c5727c00fc4744d00cc8446
Cody Quantock <codyquantock@gmail.com>
2023-06-27 23:18:28 -0400

    updating metro config

--- metro.config.js
+++ metro.config.js
@@ -1,18 +1,18 @@
+const { getDefaultConfig } = require('expo/metro-config');
+const config = getDefaultConfig(__dirname);
+
 const exclusionList = require('metro-config/src/defaults/exclusionList');
 
 // exclusionList is a function that takes an array of regexes and combines
 // them with the default exclusions to return a single regex.
 
-module.exports = {
-  resolver: {
-    blacklistRE: exclusionList([/#current-cloud-backend\/.*/]),
+
+config.resolver.blacklistRE = exclusionList([/#current-cloud-backend\/.*/])
+config.transformer.getTransformOptions = async () => ({
+  transform: {
+    experimentalImportSupport: false,
+    inlineRequires: false,
   },
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: false,
-      },
-    }),
-  },
-};
+})
+
+module.exports = config
